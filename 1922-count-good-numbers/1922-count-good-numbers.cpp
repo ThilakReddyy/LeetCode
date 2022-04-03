1caa74ef22432c4ac4bc49620c6709cf2d649a98
THILAK REDDY <64121161+ThilakReddyy@users.noreply.github.com>
2022-04-04 03:40:14 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (84.38%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.cpp
+++ 1922-count-good-numbers/1922-count-good-numbers.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
+     const int M = 1000000007;
       int countGoodNumbers(long long n) {
-        const int M = 1000000007;
         return (int)(fast_pow(5,(n+1)/2)*fast_pow(4,(n/2))%M);
     }
-    long long fast_pow(int base, long long n,int M=1000000007)
+    long long fast_pow(int base, long long n)
     {
         if(n==0)
            return 1;
