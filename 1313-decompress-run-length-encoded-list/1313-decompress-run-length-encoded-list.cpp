86b3055e76c0e11bd55afc494efc58260becde76
THILAK REDDY <64121161+ThilakReddyy@users.noreply.github.com>
2022-04-04 13:42:38 +0530

    Time: 10 ms (31.24%), Space: 10 MB (48.40%) - LeetHub

--- 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.cpp
+++ 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> decompressRLElist(vector<int>& nums) {
             int n=nums.size();
             vector<int>s;
-            for(int i=0;i<n;i=i+2)
+            for(int i=0;i<n;i+=2)
             {
                 for(int j=0;j<nums[i];j++)
                 {
