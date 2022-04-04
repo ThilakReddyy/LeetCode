16f8fcd77b20e51f9f5032a0c5f57d93018ff925
THILAK REDDY <64121161+ThilakReddyy@users.noreply.github.com>
2022-04-04 13:39:13 +0530

    Time: 13 ms (14.78%), Space: 9.9 MB (79.97%) - LeetHub

--- 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.cpp
+++ 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.cpp
@@ -5,11 +5,9 @@ public:
             vector<int>s;
             for(int i=0;i<n;i=i+2)
             {
-                int freq=nums[i];
-                int val=nums[i+1];
-                for(int j=0;j<freq;j++)
+                for(int j=0;j<nums[i];j++)
                 {
-                    s.push_back(val);
+                    s.push_back(nums[i+1]);
                 }
             }
         return s;
