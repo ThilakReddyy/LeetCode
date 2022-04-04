8c7c297b0c62fa7c63c780cd254c116ef2282f0a
THILAK REDDY <64121161+ThilakReddyy@users.noreply.github.com>
2022-04-05 03:59:06 +0530

    Time: 8 ms (83.60%), Space: 9.1 MB (61.35%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
     vector<int> minOperations(string boxes) {
-         int n=boxes.size();
-        vector<int>res;
-        for(int i=0;i<n;i++)
-        {
-            int steps=0;
-            for(int j=i;j<n;j++)
-            {
-                if(boxes[j]=='1')
-                    steps+=(j-i);
-            }
-            res.push_back(steps);
+        int n=boxes.size();
+        vector<int> ans1(n,0);
+        vector<int> ans2(n,0);
+        
+        int curr=0;
+        for(int i=0;i<n;i++){
+            if(i>0)
+                ans1[i]=ans1[i-1]+curr;
+            if(boxes[i]=='1')
+                curr++;
+        }
+        curr=0;
+        for(int i=n-1;i>=0;i--){
+            if(i<n-1)
+                ans2[i]=ans2[i+1]+curr;
+            if(boxes[i]=='1')
+                curr++;
         }
         
-        for(int i=n-1;i>=0;i--)
-        {
-            int steps=0;
-            for(int j=i;j>=0;j--)
-            {
-                if(boxes[j]=='1')
-                    steps+=(i-j);
-            }
-            res[i]+=steps;
-        }
-        return res;
+        for(int i=0;i<n;i++) ans1[i]+=ans2[i];
+        return ans1;
     }
 };
