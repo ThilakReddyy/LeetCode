2c0ebcf9fece7d030d8a45656354afaa15626a37
THILAK REDDY <64121161+ThilakReddyy@users.noreply.github.com>
2022-04-04 14:01:37 +0530

    Time: 44 ms (37.48%), Space: 13.6 MB (64.88%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -1,9 +1,12 @@
 class Solution {
 public:
     int minPartitions(string n) {
+        char p='0';
         
-        sort(n.begin(), n.end());
-        
-        return n[n.size()-1]-'0';
+        for(int i=0;i<n.size();i++)
+        {
+            if(n[i]>p)p=n[i];
+        }
+        return p-'0';
     }
 };
