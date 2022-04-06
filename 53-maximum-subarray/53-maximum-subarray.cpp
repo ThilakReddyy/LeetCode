d82bc9f54422133cdc343c6a7bc042b9f66fdfc8
THILAK REDDY <64121161+ThilakReddyy@users.noreply.github.com>
2022-04-07 04:38:43 +0530

    Time: 97 ms (94.50%), Space: 67.7 MB (91.24%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-       
-        int local=nums[0],global=nums[0];
-        for(int i=1;i<nums.size();i++){
-            local=max(local+nums[i],nums[i]);
-            global=max(global,local);
+         ios_base::sync_with_stdio(0);
+        cin.tie(0); cout.tie(0);
+        int sum=0,maxi=INT_MIN;
+        for(int it:nums)
+        {
+             sum+=it;
+            maxi=max(sum,maxi);
+            if(sum<0)sum=0;
         }
-        return global;
+        return maxi;
     }
 };
