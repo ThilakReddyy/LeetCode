13bfc6deb8c4a2c7f84e14b46a9ee0ef15a4d984
THILAK REDDY <64121161+ThilakReddyy@users.noreply.github.com>
2022-04-07 05:43:54 +0530

    Time: 214 ms (21.60%), Space: 93.5 MB (13.32%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -4,7 +4,7 @@ public:
       int maxprice=0,minprice=INT_MAX;
         for(int it:a)
         {
-          if(it<minprice)  minprice=it;
+            minprice=min(minprice,it);
             maxprice=max(maxprice,it-minprice);
         }
         return maxprice;
