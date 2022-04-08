fde0a525407dcbb43d53cdc48b28c4183b2749b3
THILAK REDDY <64121161+ThilakReddyy@users.noreply.github.com>
2022-04-08 11:11:51 +0530

    Time: 68 ms (67.47%), Space: 33 MB (72.91%) - LeetHub

--- 1603-design-parking-system/1603-design-parking-system.cpp
+++ 1603-design-parking-system/1603-design-parking-system.cpp
@@ -2,14 +2,14 @@ class ParkingSystem {
 int arr[4];
 public:
     ParkingSystem(int big, int medium, int small) {
-        this->arr[1]=big;
-        this->arr[2]=medium;
-        this->arr[3]=small;
+        arr[1]=big;
+        arr[2]=medium;
+        arr[3]=small;
     }
     
     bool addCar(int carType) {
-        if(arr[carType]<=0)return false;
-        arr[carType]-=1;
+        if(arr[carType]==0)return false;
+        arr[carType]--;
         return true;
     }
 };
