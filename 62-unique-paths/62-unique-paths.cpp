a7647fabf4061e493b14cd8b259e50053cbf2cc9
THILAK REDDY <64121161+ThilakReddyy@users.noreply.github.com>
2022-04-08 05:48:11 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (73.49%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-          int N = n + m - 2;
+          n = n + m - 2;
             m = m - 1; 
             double res = 1;
             
             for (int i = 1; i <= m; i++)
-                res = res * (N - m + i) / i;
+                res = res * (n - m + i) / i;
             return (int)res;
     }
 };
