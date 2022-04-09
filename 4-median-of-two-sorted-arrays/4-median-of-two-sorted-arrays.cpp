d075102a3d4c737b504f2a2482e399848fd847ae
THILAK REDDY <64121161+ThilakReddyy@users.noreply.github.com>
2022-04-09 11:10:26 +0530

    Time: 66 ms (30.19%), Space: 90 MB (25.76%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -14,7 +14,7 @@ public:
         if(n%2==0)
         {
             
-            return (double)((double)(v[n/2]+v[(n/2)-1])/2);
+            return ((double)(v[n/2]+v[(n/2)-1])/2);
         }
         else
         {
