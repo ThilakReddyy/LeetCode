46be168ae0cabdcdf1a4103706776aaf5f415316
THILAK REDDY <64121161+ThilakReddyy@users.noreply.github.com>
2022-04-16 20:22:14 +0530

    Time: 3 ms (28.57%), Space: 5.9 MB (77.91%) - LeetHub

--- 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
@@ -2,10 +2,7 @@ class Solution {
 public:
     int xorOperation(int n, int start) {
         int xorr=0;
-        for(int i=0;i<n;i++)
-        {
-            xorr=xorr^(start+2*i);
-        }
+        for(int i=0;i<n;i++) xorr=xorr^(start+2*i);
         return xorr;
     }
 };
