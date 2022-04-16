641068ef5323598bf00fc27cf48f81ea12142a2a
THILAK REDDY <64121161+ThilakReddyy@users.noreply.github.com>
2022-04-16 20:17:57 +0530

    Time: 0 ms (100.00%), Space: 6 MB (25.37%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -4,15 +4,9 @@ public:
         int steps=0;
         while(num)
         {
-            if(num%2)
-            {
-                num=num-1;
-            }
-            else
-            {
-                num/=2;
-            }
-            steps++;
+            if(num%2) num=num-1;
+            else num/=2;
+            steps+=1;
         }
         return steps;
     }
